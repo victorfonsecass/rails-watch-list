ede709532ae3ff87c00e637f3d70b6f52b4e1959
victor augusto <victorfonsecass@outlook.com>
2021-11-20 18:33:39 -0300

    implementei um pocuo de javascript

---
+++ app/javascript/components/card_category.js
@@ -0,0 +1,10 @@
+import Typed from 'typed.js';
+
+const loadDynamicCardCategoryText = () => {
+  new Typed('#banner-typed-text', {
+    strings: ["Save any kind of movies"],
+    typeSpeed: 50,
+    loop: true
+  });
+}
+export { loadDynamicCardCategoryText }
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,14 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import 'bootstrap';
+import 'bootstrap'
+
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+import { loadDynamicCardCategoryText } from "../components/card_category"
+document.addEventListener('turbolinks:load', () =>{
+  loadDynamicCardCategoryText()
+});
